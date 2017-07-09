57fd01930dd67b2e3b1126ca99859746bdce720f
Fluigip <fluigipanettieri@gmail.com>
2017-07-08 22:52:07 -0300

    Reflect object

--- Assets/Cloner.cs
+++ Assets/Cloner.cs
@@ -4,49 +4,34 @@ using UnityEngine;
 
 public class Cloner : MonoBehaviour {
 
-    private Plane plane;
-    private Vector3 normal;
+  private Plane plane;
+  private Vector3 normal;
 
-
-	// Use this for initialization
 	public void Start () {
-
-        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-
-        normal = (mesh.normals[0] + mesh.normals[1]) / 2;
-
-        Debug.Log(normal);
-
-        //mesh.normals[0];
+    Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+    normal = (mesh.normals[0] + mesh.normals[1]) / 2;
 	}
 	
-	// Update is called once per frame
-	public void Update () {
-		
-	}
+	public void Update ()
+  {
+    // TODO: track updates of mirrored objects
+  }
 
-    public void OnTriggerEnter(Collider other)
-    {
-        GameObject otherGO = other.gameObject;
+  public void OnTriggerEnter(Collider collided)
+  {
+    GameObject otherGO = collided.gameObject;
+    Vector3 pos = collided.transform.position;
+    Vector3 projection = Vector3.ProjectOnPlane(pos, normal);
+    Vector3 mirrored = 2 * projection - pos;
 
-        Debug.Log("C Entro el objeto");
+    Instantiate(otherGO, mirrored, collided.transform.rotation);
+    // TODO: link the two object 
+  }
 
-        Vector3 pos = other.transform.position;       
-        
-   
-        //Debug.Log(pos);
-
-        //Reflejar bola
-        Instantiate(otherGO,-normal * 5,Quaternion.identity);
-
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        GameObject otherGO = other.gameObject;
-
-        Debug.Log("C Salio el objeto");
-
-    }
-
+  public void OnTriggerExit(Collider other)
+  {
+    GameObject otherGO = other.gameObject;
+    Debug.Log("C Salio el objeto");
+    // TODO: remove mirrored object
+  }
 }
